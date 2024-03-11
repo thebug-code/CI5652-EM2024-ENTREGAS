c353e3d4bdffdd977f7553f11373b2df71814f34
Alejandro Meneses <83224454+thebug-code@users.noreply.github.com>
2024-03-11 12:31:22 -0400

    format con black

--- tarea_7/p3/longestsubstring.py
+++ tarea_7/p3/longestsubstring.py
@@ -2,23 +2,24 @@
 
 import sys
 
+
 # Calcula el arreglo correspondiente a la funcion de prefijo de s
 def longest_border(s):
-	p = [0] * len(s)
+    p = [0] * len(s)
 
-	for i in range(1, len(s)):
-		k = p[i - 1]
-		while k > 0 and s[k] != s[i]:
-			k = p[k - 1]
-		if s[k] == s[i]:
-			k += 1
-		p[i] = k
+    for i in range(1, len(s)):
+        k = p[i - 1]
+        while k > 0 and s[k] != s[i]:
+            k = p[k - 1]
+        if s[k] == s[i]:
+            k += 1
+        p[i] = k
 
-	# Obtiene el string que es prefijo y sufijo de s
-	longest_border = s[0:p[len(s) - 1]]
-	return longest_border
+    # Obtiene el string que es prefijo y sufijo de s
+    longest_border = s[0 : p[len(s) - 1]]
+    return longest_border
 
 
 if __name__ == "__main__":
-	s = sys.argv[1]
-	print(longest_border(s))
+    s = sys.argv[1]
+    print(longest_border(s))
